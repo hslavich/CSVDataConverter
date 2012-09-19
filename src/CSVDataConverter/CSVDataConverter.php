1f595a3502c3be1a4b6192e99979413cc30866e1
hslavich <hernan.slavich@gmail.com>
2012-09-19 16:09:34 -0300

    first implementation

--- src/CSVDataConverter/CSVDataConverter.php
+++ src/CSVDataConverter/CSVDataConverter.php
@@ -1,8 +1,72 @@
 <?php
-
 namespace CSVDataConverter;
 
 class CSVDataConverter
 {
+    protected $input = array();
+    protected $row = array();
+    protected $columns = array();
+    protected $rowData = array();
+    protected $output = array();
+    protected $fila = array();
+    protected $delimiter = ',';
 
+    public function inputArray($data)
+    {
+        $this->input = $data;
+    }
+
+    protected function bindFila($row)
+    {
+        $datos = str_getcsv($row, $this->delimiter);
+        if (count($datos) != count($this->columns)) {
+            throw new Exception("");
+        }
+        $this->fila = array_combine($this->columns, $datos);
+    }
+
+    public function load()
+    {
+        foreach ($this->input as $dato) {
+            $this->bindFila($dato);
+            $row = array();
+            foreach ($this->rowData as $data) {
+                if ($data instanceof \Closure) {
+                    $row[] = $data($this);
+                } else {
+                    $row[] = $data;
+                }
+            }
+            $this->output[] = $row;
+        }
+    }
+
+    public function value($column)
+    {
+        return function($csv) use ($column) {
+            return $csv->getValue($column);
+        };
+    }
+
+    public function getValue($clave)
+    {
+        return trim($this->fila[$clave]);
+    }
+
+    public function setRowData($rowData)
+    {
+        $this->rowData = $rowData;
+    }
+
+    public function setColumns($columns)
+    {
+        $this->columns = $columns;
+    }
+
+    public function getOutputAsArray()
+    {
+        $this->load();
+
+        return $this->output;
+    }
 }
--- test/CSVDataConverterTest.php
+++ test/CSVDataConverterTest.php
@@ -1,12 +1,39 @@
 <?php
-
 use CSVDataConverter\CSVDataConverter;
 
-class CSVDataConverterTest extends \PHPUnit_Framework_TestCase
+class CSVDataConverterTest extends PHPUnit_Framework_TestCase
 {
-    public function testDummy()
+    public function testFixedOutputData()
     {
-        new CSVDataConverter();
-        $this->assertTrue(true);
+        $conv = new CSVDataConverter();
+        $conv->inputArray(array('a,1', 'b,2', 'c,3'));
+        $conv->setColumns(array('char', 'num'));
+        $conv->setRowData(array('1', '2'));
+        $output = $conv->getOutputAsArray();
+
+        $this->assertEquals(3, count($output));
+        $this->assertEquals(1, $output[0][0]);
+        $this->assertEquals(2, $output[0][1]);
+        $this->assertEquals(1, $output[1][0]);
+        $this->assertEquals(2, $output[1][1]);
+    }
+
+    public function testOutputDataEqualThanInput()
+    {
+        $conv = new CSVDataConverter();
+        $conv->inputArray(array('a,1', 'b,2', 'c,3'));
+        $conv->setColumns(array('char', 'num'));
+
+        $conv->setRowData(array($conv->value('char'), $conv->value('num')));
+
+        $output = $conv->getOutputAsArray();
+
+        $this->assertEquals(3, count($output));
+        $this->assertEquals('a', $output[0][0]);
+        $this->assertEquals(1, $output[0][1]);
+        $this->assertEquals('b', $output[1][0]);
+        $this->assertEquals(2, $output[1][1]);
+        $this->assertEquals('c', $output[2][0]);
+        $this->assertEquals(3, $output[2][1]);
     }
 }
